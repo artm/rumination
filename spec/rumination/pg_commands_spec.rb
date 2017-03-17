a434acb65db07fa06a4471f7a7e99f4bcd9bc873
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-03-17 13:12:29 +0100

    clean up pg commands spec

--- spec/rumination/pg_commands_spec.rb
+++ spec/rumination/pg_commands_spec.rb
@@ -1,14 +1,11 @@
 require "spec_helper"
 
-class Dsl
+class TestDsl
   include Rumination::Pg::Commands
-  def sh
-
-  end
 end
 
 RSpec.describe Rumination::Pg::Commands do
-  subject(:dsl) { Dsl.new }
+  subject(:dsl) { TestDsl.new }
   describe "create_dump" do
     it "has usable defaults" do
       expect(dsl).to receive(:sh).with("pg_dump --compress=9 --file=dump/path")
