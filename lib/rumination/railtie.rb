e867a9cf3ebf058da24bcac62953f42c09c4b6f2
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-03-22 14:18:11 +0100

    make sure we can provide tasks to rails apps

--- lib/rumination/railtie.rb
+++ lib/rumination/railtie.rb
@@ -2,5 +2,11 @@ require "rails/railtie"
 
 module Rumination
   class Railtie < Rails::Railtie
+    rake_tasks do
+      require "rumination/deploy"
+      Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each do |path|
+        load path
+      end
+    end
   end
 end
