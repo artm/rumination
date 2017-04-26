a778ee16905836031be6d50c2d0cc05029c144bf
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-04-26 10:21:23 +0200

    support preloading rake files

--- spec/support/rake.rb
+++ spec/support/rake.rb
@@ -3,20 +3,27 @@
 # https://robots.thoughtbot.com/test-rake-tasks-like-a-boss
 #
 require "rake"
+require "pathname"
 
 RSpec.shared_context "rake" do
+  let(:rake_load_path) { Pathname.new(File.expand_path("../../../lib/rumination/tasks", __FILE__)) }
   let(:rake) { Rake::Application.new }
   let(:task_name) { self.class.top_level_description }
-  let(:task_path) { "lib/rumination/tasks/#{task_name.split(":").first}" }
+  let(:task_path) { task_name.split(":").first }
+  let(:preload_task_files) { [] }
   subject(:task) { rake[task_name] }
 
-  def loaded_files_excluding_current_rake_file
-    $".reject {|file| file == "#{task_path}.rake" }
+  def rake_require task_path
+    already_loaded = $".reject {|file| file == "#{task_path}.rake" }
+    Rake.application.rake_require(task_path, [rake_load_path], already_loaded)
   end
 
   around(:example) do |example|
     Rake.application = rake
-    Rake.application.rake_require(task_path, ["."], loaded_files_excluding_current_rake_file)
+    preload_task_files.each do |file|
+      rake_require file
+    end
+    rake_require task_path
     Rumination.factory_reset
     Dir.chdir("spec/fixtures/client_app") do
       example.call
