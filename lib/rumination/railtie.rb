63845a948dad19584e85f8dafcbf4c5389b7dfae
Artem Baguinski <abaguinski@depraktijkindex.nl>
2017-05-04 10:25:11 +0200

    use require "rumination/tasks" in railtie
    so it doesn't load them double if caller's Railfile repeats it

--- lib/rumination/railtie.rb
+++ lib/rumination/railtie.rb
@@ -3,10 +3,7 @@ require "rails/railtie"
 module Rumination
   class Railtie < Rails::Railtie
     rake_tasks do
-      require "rumination/deploy"
-      Dir[File.expand_path("../tasks/**/*.rake", __FILE__)].each do |path|
-        load path
-      end
+      require "rumination/tasks"
     end
     initializer "rumination.load_app_env" do
       require "dotenv"
